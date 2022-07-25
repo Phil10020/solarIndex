10339a50d9403ecbcfc4f51d5d9a82a8f0397100
Phil10020 <pan10020@gmail.com>
2022-07-25 18:50:28 +0800

    js :component

---
+++ src/views/poblicItems/poblicBuildSystem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="contract-list d-flex flex-wrap">
+    <!-- test  -->
+    <div class="item" data-bs-toggle="modal" :data-bs-target="'#'+item.name" v-for="item in buildSystem" :key="item.title">
+      <font-awesome-icon icon="fa-solid fa-chevron-right" />&nbsp;{{ item.title }}
+      <!-- [Start]model  -->
+      <div class="modal fade" :id="item.name" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-bs-keyboard="false" data-bs-backdrop="static">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+              <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">{{  item.title }}</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body" style="height: 100vh">
+                  <img :src="item.buildImg" alt="" style="width:100%">
+              </div>
+            </div>
+        </div>
+      </div>
+      <!-- [End]model  -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'buildSystem',
+  data () {
+    return {
+      buildSystem: [
+        { title: '光電模組', name: 'a1', buildImg: require('../../../public/images/productWelfareId/buildImg1.jpeg') },
+        { title: '變流器', name: 'b2', buildImg: require('../../../public/images/productWelfareId/buildImg2.jpg') }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/views/poblicItems/poblicProgress.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="row">
+    <div class="col-md-12 d-none d-sm-block text-muted">
+    *可能因政府跑照流程或天候等因素而有調整，將依實際情形即時更新進度
+    </div>
+    <div class="col-md-12 d-none d-sm-block">
+    <div class="schedule-timeline">
+    <p class="title">期程</p>
+    <div class="timeline-data">
+        <div class="timeline-list" style="width: 100%">
+        <p>111 年</p>
+        </div>
+        <div class="dates">
+        <span v-for="(item, i) in progressData" :key="item+123" class="date" :style="{left: (i / (progressData.length - 1))*100 + '%'}">{{ item.date }}</span>
+        </div>
+    </div>
+    </div>
+
+    <div class="schedule-item schedule-danger">
+    <p class="title">客戶</p>
+    <div class="time-list">
+        <div class="items" v-for="(item, i) in progressData" :key="item+123" :style="{left: (i / (progressData.length - 1))*100 + '%'}">
+        <label>{{ item.costomer }}</label>
+        </div>
+    </div>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'poblicProgress',
+  data () {
+    return {
+      progressData: [
+        { year: '111年', date: '02/14', costomer: '併聯審查同意書' },
+        { year: '111年', date: '02/21', costomer: '能源局同意備案函文' },
+        { year: '111年', date: '06/30', costomer: '電廠開工' },
+        { year: '111年', date: '07/15', costomer: '電廠建置完成/台電掛表' },
+        { year: '111年', date: '08/15', costomer: '設備登記' },
+        { year: '111年', date: '09/15', costomer: '台電抄表結算收益' }
+      ]
+    }
+  }
+}
+</script>

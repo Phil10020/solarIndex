3d8bab0747fe1f29af5a9a8580ef80597ccb2d9f
Phil10020 <pan10020@gmail.com>
2022-07-26 18:53:51 +0800

    js :option-value-add

--- src/views/poblicWelfareId.vue
+++ src/views/poblicWelfareId.vue
@@ -158,85 +158,15 @@
               align-items-center
             "
           >
-            <label style="font-size: 18px; color: #484848" class="mr-3 mb-0">
+            <label style="font-size: 18px; color: #484848" class="me-3 mb-0">
               購買片數
             </label>
             <select
               id="slt_trial_panel"
-              class="form-controller mr-3"
+              class="form-controller me-3"
               style="width: 170px; height: 38px; vertical-align: top"
             >
-              <option value="1">1</option>
-
-              <option value="2">2</option>
-
-              <option value="3">3</option>
-
-              <option value="4">4</option>
-
-              <option value="5">5</option>
-
-              <option value="6">6</option>
-
-              <option value="7">7</option>
-
-              <option value="8">8</option>
-
-              <option value="9">9</option>
-
-              <option value="10">10</option>
-
-              <option value="11">11</option>
-
-              <option value="12">12</option>
-
-              <option value="13">13</option>
-
-              <option value="14">14</option>
-
-              <option value="15">15</option>
-
-              <option value="16">16</option>
-
-              <option value="17">17</option>
-
-              <option value="18">18</option>
-
-              <option value="19">19</option>
-
-              <option value="20">20</option>
-
-              <option value="21">21</option>
-
-              <option value="22">22</option>
-
-              <option value="23">23</option>
-
-              <option value="24">24</option>
-
-              <option value="25">25</option>
-
-              <option value="26">26</option>
-
-              <option value="27">27</option>
-
-              <option value="28">28</option>
-
-              <option value="29">29</option>
-
-              <option value="30">30</option>
-
-              <option value="31">31</option>
-
-              <option value="32">32</option>
-
-              <option value="33">33</option>
-
-              <option value="34">34</option>
-
-              <option value="35">35</option>
-
-              <option value="36">36</option>
+              <option :value="item" v-for="item in solarPanels[0]['num']" :key="`${item}`">{{ item }}</option>
             </select>
             <button class="btn btn-redwine" id="btn_trial_calculation">
               開始試算
@@ -817,7 +747,8 @@ export default {
         '收益計算機',
         '相關文件與合約'
       ],
-      num: 0
+      num: 0,
+      solarPanels: [{ name: '台灣關愛基金會', price: 23235, rate: 8.69, irrRate: 6.05, powerGeneration: 8300, num: 36 }]
     }
   },
   components: {

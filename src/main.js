43e9f0a4abefc8ebed15b42cb9e83d85e891b177
Phil10020 <pan10020@gmail.com>
2022-09-28 18:28:53 +0800

    Data: npm-vue3googlemap

--- src/main.js
+++ src/main.js
@@ -18,11 +18,19 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* import specific icons */
 import { fas } from '@fortawesome/free-solid-svg-icons'
 
+// vue3 google map
+import VueGoogleMaps from '@fawmi/vue-google-maps'
+
 /* add icons to the library */
 library.add(fas)
 
 const app = createApp(App)
 app.use(VueAxios, axios)
 app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyDY-TLsDy3imgioimj8-oFolszY4AfYDAk'
+  }
+})
 app.use(router)
 app.mount('#app')

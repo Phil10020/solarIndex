31bca3465abe1f6844f7ba2c3956780f55dddf1b
Phil10020 <pan10020@gmail.com>
2022-10-17 18:33:34 +0800

    html: 新增couponPage點擊按鈕展開modal

--- src/views/couponSharing.vue
+++ src/views/couponSharing.vue
@@ -168,10 +168,50 @@
       <div>
         太陽人全民電廠保有隨時修改本活動規範的權利，得不經事前通知隨時撤銷或終止相關獎勵。若發現有任何濫用、詐欺或違反太陽人全民電廠相關政策之行為，太陽人全民電廠有權進行審查及處理，並保有終止或回收日光幣之權利。
       </div>
-      <div class="share-more-detail-btn">
-        <a href="#" data-toggle="modal" data-target="#modal_couponShare"
-          >更多說明</a
-        >
+      <!-- Button trigger modal -->
+      <button type="button" class="btn share-more-detail-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        更多說明
+      </button>
+      <!-- Modal -->
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">邀請好友拿獎勵說明</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <h5>詳細規則：</h5>
+              <ol>
+                <li>每當有兩位新朋友透過您分享的序號或連結成功註冊成為太陽人會員時，您將獲得300元日光幣獎勵，新朋友也將各獲得100元日光幣。</li>
+                <li>新朋友首次完成認購太陽能板時，您將額外再獲得500元日光幣獎勵。</li>
+                <li>太陽人全民電廠隨時保留活動修改之權利。</li>
+              </ol>
+            </div>
+            <div class="modal-footer justify-content-start">
+              <h5>注意事項：</h5>
+              <ol>
+                <li>
+                  邀請者:
+                  <ul style="list-style-type: lower-alpha;">
+                    <li>日光幣僅限於太陽人全民電廠之電廠專案認購太陽能板時使用，每片太陽能板的日光幣使用上限為500元。</li>
+                    <li>受邀者一旦完成優惠分享流程，太陽人即自動將日光幣獎勵派發至邀請者之日光幣帳戶。</li>
+                    <li>日光幣不得移轉至其他帳戶，亦不得轉換為現金或其他使用。</li>
+                    <li>若受邀者收到多個分享碼，僅受邀者首先輸入之分享碼生效，並使該邀請者獲得日光幣。</li>
+                  </ul>
+                </li>
+                <li>
+                  受邀者:
+                  <ul style="list-style-type: lower-alpha;">
+                    <li>限過去未於太陽人全民電廠註冊之使用者可享有此優惠方案之服務與獎勵。</li>
+                    <li>每位受邀者僅能使用一組分享碼。</li>
+                    <li>太陽人全民電廠隨時保留活動修改之權利，若發現有任何濫用、詐欺或違反太陽人全民電廠相關政策之行為，太陽人全民電廠有權進行審查及處理，並保有終止或回收日光幣之權利。</li>
+                  </ul>
+                </li>
+              </ol>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
